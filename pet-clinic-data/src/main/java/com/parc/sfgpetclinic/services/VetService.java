99ceebdd472a77c222b260061a7e6f411838cdae
David Stewart <davidj_stewart@yahoo.com>
2018-08-02 11:10:03 +0100

    refactoring Services to remove duplicate code

--- pet-clinic-data/src/main/java/com/parc/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/parc/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.parc.sfgpetclinic.services;
 
 import com.parc.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
