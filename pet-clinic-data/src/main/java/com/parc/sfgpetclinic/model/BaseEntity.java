c51b12e08c087113cb87a709fc33ac5629b31759
Paul Cavanagh <18164174+paulcavanagh@users.noreply.github.com>
2018-08-09 14:31:02 +0100

    updated base entity with JPA annotions

--- pet-clinic-data/src/main/java/com/parc/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/parc/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package com.parc.sfgpetclinic.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
